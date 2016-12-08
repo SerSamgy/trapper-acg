22f9d5194ffe0fcfba70c8984c756e5dc98ec920
Semyon Gaivoronskiy <samgy7cool@gmail.com>
2016-12-08 17:26:36 +0300

    Project design fixes

--- src/dice_pool.py
+++ src/dice_pool.py
@@ -3,6 +3,10 @@ import collections
 Ability = collections.namedtuple('Ability', 'dice, modifier')
 
 
+class NoRequiredFields(Exception):
+    pass
+
+
 class DicePool(list):
     def __init__(self, seq=None):
         if seq:
@@ -10,28 +14,29 @@ class DicePool(list):
         else:
             seq = ()
 
-        super(DicePool, self).__init__(seq)
+        super().__init__(seq)
 
     def append(self, p_object):
-        if not self.__iscorrect_type(p_object):
-            raise TypeError
+        if not self.__hasrequired_fields(p_object):
+            raise NoRequiredFields
 
-        super(DicePool, self).append(p_object)
+        super().append(p_object)
 
     def extend(self, iterable):
         self.__check_sequence_objects_type(iterable)
 
-        super(DicePool, self).extend(iterable)
+        super().extend(iterable)
 
     def insert(self, index, p_object):
-        if not self.__iscorrect_type(p_object):
-            raise TypeError
+        if not self.__hasrequired_fields(p_object):
+            raise NoRequiredFields
 
-        super(DicePool, self).insert(index, p_object)
+        super().insert(index, p_object)
 
     def __check_sequence_objects_type(self, seq):
-        if any(not self.__iscorrect_type(x) for x in seq):
-            raise TypeError
+        if any(not self.__hasrequired_fields(x) for x in seq):
+            raise NoRequiredFields
 
-    def __iscorrect_type(self, check_object):
-        return isinstance(check_object, Ability)
+    def __hasrequired_fields(self, check_object):
+        return hasattr(check_object, 'dice') and hasattr(check_object,
+                                                         'modifier')
