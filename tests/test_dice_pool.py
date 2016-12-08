c194e8e74e4c0b318e3801573870e8e225e66b4e
Semyon Gaivoronskiy <samgy7cool@gmail.com>
2016-12-08 17:26:36 +0300

    Project design fixes

--- tests/test_dice_pool.py
+++ tests/test_dice_pool.py
@@ -1,14 +1,16 @@
 import pytest
 
-from src import dice, dice_pool
+import dice
+import dice_pool
 
 
 def test_ability_class_has_proper_fields(ability_class):
-    assert ability_class._fields == ('dice', 'modifier')
+    assert hasattr(ability_class, 'dice')
+    assert hasattr(ability_class, 'modifier')
 
 
 def test_dice_pool_can_contain_only_ability_objects(pool_class, melee_ability):
-    with pytest.raises(TypeError):
+    with pytest.raises(dice_pool.NoRequiredFields):
         pool_class([1])
 
     new_pool = pool_class([melee_ability])
@@ -16,7 +18,7 @@ def test_dice_pool_can_contain_only_ability_objects(pool_class, melee_ability):
 
 
 def test_dice_pool_appends_only_ability_objects(pool, melee_ability):
-    with pytest.raises(TypeError):
+    with pytest.raises(dice_pool.NoRequiredFields):
         pool.append('ability')
 
     pool.append(melee_ability)
@@ -24,7 +26,7 @@ def test_dice_pool_appends_only_ability_objects(pool, melee_ability):
 
 
 def test_dice_pool_extends_only_by_ability_objects(pool, melee_ability):
-    with pytest.raises(TypeError):
+    with pytest.raises(dice_pool.NoRequiredFields):
         pool.extend(['ability'])
 
     pool.extend([melee_ability])
@@ -32,7 +34,7 @@ def test_dice_pool_extends_only_by_ability_objects(pool, melee_ability):
 
 
 def test_dice_pool_insert_only_ability_objects(pool, melee_ability):
-    with pytest.raises(TypeError):
+    with pytest.raises(dice_pool.NoRequiredFields):
         pool.insert(0, 'ability')
 
     pool.insert(1, melee_ability)
@@ -56,5 +58,5 @@ def pool_class():
 
 
 @pytest.fixture
-def pool():
-    return dice_pool.DicePool()
+def pool(pool_class):
+    return pool_class()
