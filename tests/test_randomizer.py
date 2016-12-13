f363e5c677ebc8327f5ecbf7862d36baada8a72f
Semyon Gaivoronskiy <samgy7cool@gmail.com>
2016-12-13 19:32:20 +0300

    Rewrite randomizer functions and remove unnecessary shuffling.

--- tests/test_randomizer.py
+++ tests/test_randomizer.py
@@ -1,66 +1,63 @@
 import pytest
 
+import dice
+import randomizer
 
-# FIXME: uncommit this and fix tests
-# def test_add_ability_to_pool(pool, ability_d4):
-#     randomizer.add_ability_to_pool(pool, ability_d4)
-#     assert len(pool) == 1
-#
-#
-# def test_remove_ability_from_pool(pool_class, ability_d4):
-#     new_pool = pool_class([ability_d4])
-#     initial_len = len(new_pool)
-#
-#     randomizer.remove_ability_from_pool(new_pool, ability_d4)
-#     assert len(new_pool) < initial_len
-#
-#
-# def test_empty_pool(pool_class, ability_d4, ability_d8):
-#     new_pool = pool_class([ability_d4, ability_d8])
-#
-#     randomizer.empty_pool(new_pool)
-#     assert len(new_pool) == 0
-#
-#
-# def test_shuffle_ability_dice(dice_d4, ability_d4):
-#     ability_dice = ability_d4.dice
-#
-#     randomizer.shuffle_ability_dice(ability_dice)
-#     assert ability_dice != dice_d4
-#
-#
-# def test_shuffle_pool(pool_class, ability_d4, ability_d8, dice_d4, dice_d8):
-#     new_pool = pool_class([ability_d4, ability_d8])
-#
-#     randomizer.shuffle_pool_dices(new_pool)
-#     assert ability_d4.dice != dice_d4
-#     assert ability_d8.dice != dice_d8
-#
-#
-# def test_get_random_ability_value(ability_d4):
-#     random_value = randomizer.get_random_ability_value(ability_d4)
-#
-#     assert random_value in range(1, 6)
-#
-#
-# def test_get_sum_of_random_values_from_pool(pool_class, ability_d4,
-#                                             ability_d4_without_modifier):
-#     new_pool = pool_class([ability_d4, ability_d4_without_modifier])
-#
-#     sum_from_pool = randomizer.get_sum_of_random_values_from_pool(new_pool)
-#     assert sum_from_pool in range(2, 10)
+
+def test_add_ability_to_pool(ability_d4):
+    randomizer.add_ability_to_pool(ability_d4)
+    assert len(randomizer.Pool) == 1
+
+
+def test_remove_ability_from_pool(ability_d4):
+    initial_len = len(randomizer.Pool)
+
+    randomizer.remove_ability_from_pool(ability_d4)
+    assert len(randomizer.Pool) < initial_len
+
+
+def test_empty_pool(ability_d4, ability_d8):
+    randomizer.add_ability_to_pool(ability_d4)
+    randomizer.add_ability_to_pool(ability_d8)
+
+    randomizer.empty_pool()
+    assert len(randomizer.Pool) == 0
+
+
+def test_pop_value_from_pool(ability_d8):
+    randomizer.add_ability_to_pool(ability_d8)
+
+    pop_value = randomizer.pop_value_from_pool()
+    assert pop_value == ability_d8
+    assert len(randomizer.Pool) == 0
+
+
+def test_get_random_ability_value(ability_d4):
+    random_value = randomizer.get_random_ability_value(ability_d4)
+
+    assert random_value in range(1, 6)
+
+
+def test_get_sum_of_random_values_from_pool(ability_d4,
+                                            ability_d4_without_modifier):
+    randomizer.add_ability_to_pool(ability_d4)
+    randomizer.add_ability_to_pool(ability_d4_without_modifier)
+
+    sum_from_pool = randomizer.get_sum_of_random_values_from_pool()
+    assert sum_from_pool in range(2, 10)
+    assert len(randomizer.Pool) == 0
 
 
 @pytest.fixture
-def ability_d4(dice_d4, ability_class):
-    return ability_class(dice_d4.copy(), 1)
+def ability_d4(ability_class):
+    return ability_class(dice.d4, 1)
 
 
 @pytest.fixture
-def ability_d4_without_modifier(dice_d4, ability_class):
-    return ability_class(dice_d4.copy(), 0)
+def ability_d4_without_modifier(ability_class):
+    return ability_class(dice.d4, 0)
 
 
 @pytest.fixture
-def ability_d8(dice_d8, ability_class):
-    return ability_class(dice_d8.copy(), 2)
+def ability_d8(ability_class):
+    return ability_class(dice.d8, 2)
